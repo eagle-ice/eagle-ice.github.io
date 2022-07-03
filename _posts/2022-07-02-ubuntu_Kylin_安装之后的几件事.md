807be6e1c71c0cfdf491f0cfa1ae703479115c07
eagle_ice <eagle-ice@163.com>
2022-07-03 13:42:06 +0800

    修改主题并完成创建博客的脚本
    todo: 创建blog的脚本完成的创建的部分
    还有标签和类型的选择没写

--- _posts/2022-07-02-ubuntu_Kylin_安装之后的几件事.md
+++ _posts/2022-07-02-ubuntu_Kylin_安装之后的几件事.md
@@ -1,14 +1,13 @@
 ---
-
 layout: post
-title: 如何是真4
+title: ubuntu_Kylin 安装之后的几件事
 tags:
 - tools
-categories: Linux VM
-description: Jacman 是一款扁平化，有着响应式设计的 Jekyll 主题。本站正式使用了 Jacman 主题。Jacman 基于 Jacman 的 Hexo 主题修改而来。你可以前往本站和 Demo 预览更多关于本主题的更多效果。如果你有任何问题或意见欢迎到 GitHub 发表 issue。 
+- VMPlayer
+categories: Software Linux
+description: 这是一些说明
 ---
-
-# ubuntu_Kylin 安装之后的几件事
+系统新装之后基础软件的安装
 
 ## 1. 切换HOME
 
---
+++ create_blog.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+file_path=_posts
+function help_fun()
+{
+    echo "ex:./create_blog.sh \"title\" \"categories\" \"tags\""
+    exit
+}
+
+#categories_list=
+#tags_list=
+blog_head_template="---
+title: 
+date: 
+link:
+tags:
+categories:
+description:
+---
+"
+
+blog_head_template1="---"
+function create_blog_file()
+{
+    if [ ! -f ${1} ]; then
+        echo "create ${1}"
+        echo "$blog_head_template" > ${1}
+    else
+        #while read line
+        #do
+        #    echo "$line"
+        #    if [ x$line != x"---" ];then
+        #        sed -i "1 i ${blog_head_template[0]}" $1
+        #    fi
+        #    break
+        #done < $1
+        echo "Please re-enter the file if it already exists!!"
+        echo "file_name = $1"
+        exit -1
+    fi
+}
+function create_blog_head()
+{
+    echo "title = $1"
+    echo "categories = $2"
+    echo "tags = $3"
+    echo "file_name = $4"
+    sed -i "s/date:.*/date: $(date +"%Y-%m-%d %H:%M:%S")/g" $4
+    sed -i "s/categories:.*/categories: $2/g" $4
+    sed -i "s/tags:.*/tags: $3/g" $4
+}
+
+title=$1
+categories=$2
+tags=$3
+echo "$# ${categories} ${tags}"
+if [ $# -ne 3 ]; then
+    help_fun
+fi
+file_name=`date +"%Y-%m-%d"`-${title}.md
+
+create_blog_file "${file_path}/${file_name}"
+create_blog_head "$title" "$categories" "$tags" "${file_path}/$file_name"
